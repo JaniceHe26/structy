9cebbb12d6e0b7c44143255ce20c5e680fb9067d
Janice He <aikeiti@dominics-mbp.mynetworksettings.com>
2023-02-10 19:05:32 -0500

    fff

--- linkedlist.js
+++ linkedlist.js
@@ -38,3 +38,115 @@ b.next = c;
 c.next = d;
 
 console.log(printLinkedList(a));
+
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+const linkedListValues = (head) => {
+  // todo
+  if (head === null) return [];
+  
+  const results = [];
+  let curr = head;
+  
+  while(curr !== null) {
+     results.push(curr.val);
+     curr = curr.next;
+  }
+  
+  return results;
+};
+
+module.exports = {
+  linkedListValues,
+};
+
+
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+const sumList = (head) => {
+  // todo
+  if (head === null) return 0;
+  let sum = 0;
+  let curr = head;
+  
+  while (curr !== null) {
+    sum += curr.val;
+    curr = curr.next;
+  }
+  
+  return sum;
+};
+
+module.exports = {
+  sumList,
+};
+
+
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+const linkedListFind = (head, target) => {
+  // todo
+  
+  if (head === null) return false;
+  
+  let curr = head;
+  
+  while(curr !== null) {
+    if (curr.val === target) {
+      return true;
+    }
+    curr = curr.next;
+  }
+  
+  return false;
+};
+
+module.exports = {
+  linkedListFind,
+};
+
+
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+const getNodeValue = (head, index) => {
+  // todo
+  
+  if (head === null) return null;
+  
+  let nodeArr = [];
+  let curr = head;
+  
+  while (curr !== null) {
+    nodeArr.push(curr.val);
+    curr = curr.next;
+  }
+  
+  if (nodeArr.length < index) return null;
+  
+  return nodeArr[index];
+};
+
+module.exports = {
+  getNodeValue,
+};
+
