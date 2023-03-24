83291f1f490674779155a226d3ae4fbedc78bcdd
Janice He <aikeiti@dominics-mbp.mynetworksettings.com>
2023-03-23 22:46:18 -0400

    ff

--- lc/string/1961.js
+++ lc/string/1961.js
@@ -5,34 +5,47 @@
  */
 var isPrefixString = function(s, words) {
 
+  let prefix = ""
+  for (const word of words) {
+      prefix += word;
 
-  let index = 0;
-  let startIndexInSubStr = 0;
-
-  while (index < words.length) {
-      if (startIndexInSubStr > s.length) {
+      if (prefix.length > s.length) {
           return false;
       }
-
-      if (startIndexInSubStr === s.length) {
-          return true;
-      } 
-
-      //i === i, we want to look at the next word and and the next substr start length
-      if (words[index] !== s.substring(startIndexInSubStr, startIndexInSubStr + words[index].length)) {   
-          return false;
+      if (prefix.length === s.length) {
+          return prefix === s;
       }
-      startIndexInSubStr += words[index].length;
-      index++;
   }
 
-
-
-  if (startIndexInSubStr === s.length) {
-      return true;
-  } 
-  
   return false;
-};
 
+
+  // let index = 0;
+  // let startIndexInSubStr = 0;
+
+  // while (index < words.length) {
+  //     if (startIndexInSubStr > s.length) {
+  //         return false;
+  //     }
+
+  //     if (startIndexInSubStr === s.length) {
+  //         return true;
+  //     } 
+
+  //     //i === i, we want to look at the next word and and the next substr start length
+  //     if (words[index] !== s.substring(startIndexInSubStr, startIndexInSubStr + words[index].length)) {   
+  //         return false;
+  //     }
+  //     startIndexInSubStr += words[index].length;
+  //     index++;
+  // }
+
+
+
+  // if (startIndexInSubStr === s.length) {
+  //     return true;
+  // } 
+
+  // return false;
+};
 
